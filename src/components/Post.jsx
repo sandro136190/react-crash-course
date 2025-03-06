49f5e1d85cd2215e53e5c1e3968ea81dd27d8fa3
sandro136190 <sa.bochorishvili@gmail.com>
2025-03-06 22:58:35 +0400

    first custom component

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <div>
+      <p>Maxximilian</p>
+      <p>Resct.js isnot awesome!</p>
+    </div>
+  );
+};
+
+export default Post;
