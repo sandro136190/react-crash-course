94f39e7531deadec4c876781c593b8d4a3fc1916
sandro136190 <sa.bochorishvili@gmail.com>
2025-03-06 23:15:34 +0400

    Dynamic value

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+const names = ["Otari", "Vaja"];
 
 const Post = () => {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Maxximilian</p>
+      <p>{chosenName}</p>
       <p>Resct.js isnot awesome!</p>
     </div>
   );
