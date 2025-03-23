0b9438ea62a1b42eb4a281c490be96d6dbb8f2d9
sandro136190 <sa.bochorishvili@gmail.com>
2025-03-23 16:50:19 +0400

    Fix isFetching state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -29,14 +29,14 @@ const PostsList = ({}) => {
   }
   return (
     <>
-      {isFetching && posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {isFetching && posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts</h2>
           <p>If u want to see if it works please add some posts</p>
